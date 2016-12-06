422043ada043edad09142883c48166fafba1b7f7
Travis Deshotels <travis.deshotels@gmail.com>
2016-12-05 20:17:13 -0500

    Update so I can change branches.

--- RestaurantApp/src/main/webapp/createorder.js
+++ RestaurantApp/src/main/webapp/createorder.js
@@ -1,20 +1,21 @@
 var app = angular.module('myApp',[]);
-app.controller('testCtrl',function testCtrl($scope){
+app.controller('testCtrl', ['$scope', function($scope){
 	$scope.showTheBox = true;
 	$scope.processForm = function() {
 		$scope.showTheBox = !$scope.showTheBox;
-	}
+	};
 	$scope.orderitem = {
 		quantity : 0,
 		price : 0,
 		itemname : ""
-	}
+	};
 	$scope.order = {
 		orderitemlist = [],
 		total = 0
-	}
+	};
 	$scope.addtoorder = function(orderItem) {
 		$scope.order.orderlist.push(orderItem);
 		$scope.order.total += $scope.orderItem.price;
-	}
-});
+	};
+}]);
+
