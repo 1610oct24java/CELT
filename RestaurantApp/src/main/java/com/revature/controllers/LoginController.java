f76e732ffce0bad38df851d33feebfd4e1c02449
Travis Deshotels <travis.deshotels@gmail.com>
2016-12-09 18:52:07 -0500

    Add working login, but broken redirect.

--- RestaurantApp/src/main/java/com/revature/controllers/LoginController.java
+++ RestaurantApp/src/main/java/com/revature/controllers/LoginController.java
@@ -2,9 +2,11 @@ package com.revature.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.revature.beans.Customer;
 import com.revature.beans.Manager;
@@ -16,8 +18,8 @@ import com.revature.dao.UserDAOImp;
 @RequestMapping(value = "/login")
 public class LoginController {
 	@RequestMapping(method=RequestMethod.POST)
-	public String login(HttpServletRequest request){
-		String direct = "index.html";
+	public String doLogin(HttpServletRequest request){
+		String direct = "login.html";
 		UserDAO userDAO = new UserDAOImp();
 		Manager manager = null;
 		Customer customer = null;
@@ -28,15 +30,20 @@ public class LoginController {
 			if (manager != null){
 				// user is a manager
 				request.getSession().setAttribute("user", manager);
-				direct = "restaurant.html";
+				direct = "pages/restaurant.html";
 			}
 			else{
 				// user is a customer
 				customer = userDAO.getCustomerByID(user.getUserId());
 				request.getSession().setAttribute("user", customer);
-				direct = "customer.html";
+				direct = "pages/customer.html";
 			}
 		}
-		return "redirect:/" + direct;
+		return "redirect:" + direct;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String getLogin(){
+		return "redirect:pages/login.html";
 	}
 }
