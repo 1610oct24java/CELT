b124da0d5e228850cae325f686cea0e58e3a7960
Travis Deshotels <travis.deshotels@gmail.com>
2016-12-11 15:02:04 -0500

    Merge RestaurantDAO methods.

--- RestaurantApp/src/main/java/com/revature/dao/RestaurantDAOImp.java
+++ RestaurantApp/src/main/java/com/revature/dao/RestaurantDAOImp.java
@@ -4,10 +4,36 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import com.revature.beans.ContactInfo;
+import com.revature.beans.Manager;
+import com.revature.beans.Restaurant;
 import com.revature.util.HibernateUtil;
 
 public class RestaurantDAOImp implements RestaurantDAO{
 
+	@Override
+	public void saveRestaurant(ContactInfo info, Restaurant res, Manager man)
+	{
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		res.setAddress(info);
+		man.setRestaurant(res);
+		session.save(man);
+		
+		tx.commit();
+		session.close();	
+	}
+
+	@Override
+	public void updateRestaurant(Restaurant res) {
+		
+	}
+
+	@Override
+	public void deleteRestaurant(Restaurant res) {
+	
+	}
+	
 	@Override
 	public int changeStatus(int restaurantId) {
 		int rowsUpdated = 0;
